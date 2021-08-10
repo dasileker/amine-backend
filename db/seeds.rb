797ceb255de143c08d8ffc3d1d3fd8c4182a2503
Amine_Zerradi <dasileker@gmail.com>
2021-08-10 14:38:02 +0100

    remove unused files + migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Doctor.create(name: 'Dr Alan', qualification: 'Cardiologist', experience: '6 Years', image: 'https://th.bing.com/th/id/OIP.dVGBdwNEEvR8oUS67p5vXwHaFw?pid=ImgDet&rs=1')
+Doctor.create(name: 'Dr sam', qualification: 'Gastrnomie', experience: '10 Years', image: 'https://th.bing.com/th/id/R.0e8322ca32cc6016c50c5b244ed81029?rik=yAehzrE6nT6k%2bA&pid=ImgRaw&r=0')
+Doctor.create(name: 'Dr deen', qualification: 'Plumonologist', experience: '8 Years', image: 'https://www.tuftsmedicalcenter.org/-/media/Images/Physicians/Dupee_Richard.ashx')
+Doctor.create(name: 'Dr crowly', qualification: 'Neurologist', experience: '3 Years', image: 'https://profile-images.xing.com/images/e4e4e7ccea52ed7ecafef71f15280a85-1/dr-john-g-kennedy.1024x1024.jpg')
+Doctor.create(name: 'Dr freeman', qualification: 'Cardiologist', experience: '11 Years', image: 'https://www.zahngesundheit-frechen.de/wp-content/uploads/2017/04/Juergen_Schmitz-650x490.jpg')
